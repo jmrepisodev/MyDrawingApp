473d5638e3f6131f6fc10e3aca3cdf3d2cc642f1
jmrepisodev <jmrepisodev@gmail.com>
2023-01-30 23:13:25 +0100

    Ampliación de funciones y corrección de errores

--- app/src/main/java/com/repiso/mydrawingapp/DrawingView.java
+++ app/src/main/java/com/repiso/mydrawingapp/DrawingView.java
@@ -2,31 +2,39 @@ package com.repiso.mydrawingapp;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 
+/**
+ * Vista personalizada en la que se realizará el dibujo. Hereda de View.
+ */
 public class DrawingView extends View {
 
-    //trazo del dibujo
+    //Trazo de dibujo
     private Path drawPath;
-    //dibujo y pincel del lienzo
+    //Pinceles
     private Paint drawPaint, canvasPaint;
-    //color inicial
-    private int paintColor = 0xFF660000;
-    //lienzo
+    //Color inicial, color por defecto
+    private int paintColor = 0xFF660000, paintAlpha = 255;
+    //Lienzo o superficie de dibujo
     private Canvas drawCanvas;
-    //mapa de bits del lienzo
+    //La clase Canvas se almacena dentro de un mapa de bits,
+    // con unas dimensiones determinadas y en una determinada posición de la pantalla
     private Bitmap canvasBitmap;
-    //talla de los pinceles
+    //Tamaños de brocha
     private float brushSize, lastBrushSize;
+    //Bandera borrador. Inicialmente el usuario está dibujando, no borrando
     private boolean erase=false;
 
     /**
@@ -39,31 +47,31 @@ public class DrawingView extends View {
         setupDrawing();
     }
 
+    /**
+     * Establece la configuración del área de dibujo para la interacción con el usuario
+     */
     private void setupDrawing(){
 
+        //prepare for drawing and setup paint stroke properties
         brushSize = getResources().getInteger(R.integer.medium_size);
         lastBrushSize = brushSize;
-        //Trazo y pincel
+
+        //Instanciamos el trazo y el pincel
         drawPath = new Path();
         drawPaint = new Paint();
-        //color inicial
+        //Configuramos las propiedades iniciales del pincel
         drawPaint.setColor(paintColor);
-
         drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(20);
+        drawPaint.setStrokeWidth(brushSize);
         drawPaint.setStyle(Paint.Style.STROKE);
         drawPaint.setStrokeJoin(Paint.Join.ROUND);
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
-
-        drawPaint.setStrokeWidth(brushSize);
-
+        //Instanciamos el pincel del lienzo
         canvasPaint = new Paint(Paint.DITHER_FLAG);
-
-
     }
 
     /**
-     * Estable el tamaño de la vista
+     * Tamaño asignado a la Vista
      * @param w Current width of this view.
      * @param h Current height of this view.
      * @param oldw Old width of this view.
@@ -72,22 +80,28 @@ public class DrawingView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        this.canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        this.drawCanvas = new Canvas(canvasBitmap);
+        //Creamos un objeto Bitmap: dimensiones, formato de color
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        //Definimos el lienzo o superficie de dibujo, a partir de un objeto Bitmap
+        drawCanvas = new Canvas(canvasBitmap);
+
+        //*Nota: Lo recomendable es obtener el Canvas a partir de View.onDraw() o SurfaceHolder
     }
 
     /**
-     * Permite que la clase funcione como una vista de dibujo
+     * Método principal: Dibuja la vista sobre el Canvas
      * @param canvas the canvas on which the background will be drawn
      */
     @Override
     protected void onDraw(Canvas canvas) {
+        //Dibuja una imagen o mapa de bits
         canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        //Asignamos el trazo y el pincel
         canvas.drawPath(drawPath, drawPaint);
     }
 
     /**
-     * Detecta los movimientos táctiles sobre la pantalla
+     * Registra los movimientos táctiles del usuario en la pantalla
      * @param event The motion event.
      * @return
      */
@@ -95,7 +109,7 @@ public class DrawingView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         float touchX = event.getX();
         float touchY = event.getY();
-
+        //respond to down, move and up events
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 drawPath.moveTo(touchX, touchY);
@@ -104,64 +118,97 @@ public class DrawingView extends View {
                 drawPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
+                drawPath.lineTo(touchX, touchY);
                 drawCanvas.drawPath(drawPath, drawPaint);
                 drawPath.reset();
                 break;
             default:
                 return false;
         }
-
-        //Fuerza la ejecución del método
+        //Redibuja la vista
         invalidate();
         return true;
+
     }
 
-
     /**
-     * Establece el color
+     * Actualiza el color
      * @param newColor
      */
     public void setColor(String newColor){
         invalidate();
-        this.paintColor = Color.parseColor(newColor);
-        drawPaint.setColor(paintColor);
+        //check whether color value or pattern name
+        if(newColor.startsWith("#")){
+            paintColor = Color.parseColor(newColor);
+            drawPaint.setColor(paintColor);
+            drawPaint.setShader(null);
+        }
+        else{
+            //pattern
+            int patternID = getResources().getIdentifier(
+                    newColor, "drawable", "com.repiso.mydrawingapp");
+            //decode
+            Bitmap patternBMP = BitmapFactory.decodeResource(getResources(), patternID);
+            //create shader
+            BitmapShader patternBMPshader = new BitmapShader(patternBMP,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+            //color and shader
+            drawPaint.setColor(0xFFFFFFFF);
+            drawPaint.setShader(patternBMPshader);
+        }
     }
 
-    /**
-     * Establece el tamaño del pincel
-     * @param newSize
-     */
+    //set brush size
     public void setBrushSize(float newSize){
-     float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
                 newSize, getResources().getDisplayMetrics());
-        this.brushSize=pixelAmount;
-        this.drawPaint.setStrokeWidth(brushSize);
+        brushSize=pixelAmount;
+        drawPaint.setStrokeWidth(brushSize);
     }
 
+    //get and set last brush size
     public void setLastBrushSize(float lastSize){
-        this.lastBrushSize=lastSize;
+        lastBrushSize=lastSize;
     }
     public float getLastBrushSize(){
         return lastBrushSize;
     }
 
-
-    /**
-     * Establece el borrador
-     * @param isErase
-     */
+    //set erase true or false
     public void setErase(boolean isErase){
-        this.erase=isErase;
-        if(erase) {
-            drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        }
-        else{
-            drawPaint.setXfermode(null);}
+        erase=isErase;
+        //modifica el objeto Paint para borrar o para dibujar:
+        if(erase) drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        else drawPaint.setXfermode(null);
     }
 
+    /**
+     * Comenzar nuevo dibujo
+     */
     public void startNew(){
         drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
         invalidate();
     }
 
+    //return current alpha
+    public int getPaintAlpha(){
+        return Math.round((float)paintAlpha/255*100);
+    }
+
+    //set alpha
+    public void setPaintAlpha(int newAlpha){
+        paintAlpha=Math.round((float)newAlpha/100*255);
+        drawPaint.setColor(paintColor);
+        drawPaint.setAlpha(paintAlpha);
+    }
+
+    public void setPattern(String newPattern){
+        invalidate();
+        int patternID = getResources().getIdentifier(newPattern, "drawable", "com.repiso.mydrawingapp");
+        Bitmap patternBMP = BitmapFactory.decodeResource(getResources(), patternID);
+        BitmapShader patternBMPshader = new BitmapShader(patternBMP,
+                Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        drawPaint.setColor(0xFFFFFFFF);
+        drawPaint.setShader(patternBMPshader);
+    }
 }
--- app/src/main/java/com/repiso/mydrawingapp/MainActivity.java
+++ app/src/main/java/com/repiso/mydrawingapp/MainActivity.java
@@ -11,20 +11,25 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Vista personalizada
     private DrawingView drawView;
-    //Tamaños de pincel
+    //Botones
+    private ImageButton currPaint, drawBtn, eraseBtn, newBtn, saveBtn, opacityBtn;
+    //Tamaños y tallas
     private float smallBrush, mediumBrush, largeBrush;
-
-    private ImageButton currPaint, drawBtn, eraseBtn, newBtn, saveBtn;
 
 
 
@@ -33,62 +38,86 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       //Vista de dibujo y paleta de colores
         drawView = (DrawingView)findViewById(R.id.drawing);
         LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
-        //color de la paleta seleccionado por defecto
+
+        //Obtiene el primer elemento del Layout de colores
         currPaint = (ImageButton)paintLayout.getChildAt(0);
+        //Usaremos una imagen dibujable diferente en el botón para mostrar que está actualmente seleccionado
         currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
 
+        //Obtiene las dimensiones de los pinceles
         smallBrush = getResources().getInteger(R.integer.small_size);
         mediumBrush = getResources().getInteger(R.integer.medium_size);
         largeBrush = getResources().getInteger(R.integer.large_size);
 
+       //Botones de dibujo
         drawBtn = (ImageButton)findViewById(R.id.draw_btn);
         drawBtn.setOnClickListener(this);
 
-        //Tamaño de pincel por defecto
+        //Tamaño inicial del pincel
         drawView.setBrushSize(mediumBrush);
 
+        //erase button
         eraseBtn = (ImageButton)findViewById(R.id.erase_btn);
         eraseBtn.setOnClickListener(this);
 
+        //new button
         newBtn = (ImageButton)findViewById(R.id.new_btn);
         newBtn.setOnClickListener(this);
 
+        //save button
         saveBtn = (ImageButton)findViewById(R.id.save_btn);
         saveBtn.setOnClickListener(this);
+
+        //opacity
+        opacityBtn = (ImageButton)findViewById(R.id.opacity_btn);
+        opacityBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     /**
-     * Método para seleccionar el color
+     * Método para seleccionar el color elegido por el usuario
      * @param view
      */
     public void paintClicked(View view){
+
+        //Desactiva el borrador
+        drawView.setErase(false);
+        drawView.setPaintAlpha(100);
+        drawView.setBrushSize(drawView.getLastBrushSize());
+
         if(view!=currPaint){
-            drawView.setErase(false);
-
             ImageButton imgView = (ImageButton)view;
+            //Recupera la etiqueta que configuramos para cada botón en el diseño, que representa el color elegido
             String color = view.getTag().toString();
+            //Establece el color seleccionado
             drawView.setColor(color);
-
+            //Selecciona el botón presionado y actualiza la interfaz de usuario
             imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
             currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
             currPaint=(ImageButton)view;
-
-            drawView.setBrushSize(drawView.getLastBrushSize());
         }
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View view){
 
         if(view.getId()==R.id.draw_btn){
+            //Se mostrará un cuadro de diálogo con los tres tamaños posibles
             final Dialog brushDialog = new Dialog(this);
-            brushDialog.setTitle("Brush size:");
+            brushDialog.setTitle("Tamaño pincel:");
             brushDialog.setContentView(R.layout.brush_chooser);
 
+            //listen for clicks on size buttons
             ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
-
             smallBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
@@ -120,16 +149,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     brushDialog.dismiss();
                 }
             });
-
+            //show and wait for user interaction
             brushDialog.show();
         }
-
         else if(view.getId()==R.id.erase_btn){
-            //cuadro de dialogo que permite elegir el tamaña de borrador
+            //switch to erase - choose size
             final Dialog brushDialog = new Dialog(this);
-            brushDialog.setTitle("Eraser size:");
+            brushDialog.setTitle("Tamaño borrador:");
             brushDialog.setContentView(R.layout.brush_chooser);
 
+            //size buttons
             ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
             smallBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
@@ -139,6 +168,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     brushDialog.dismiss();
                 }
             });
+
             ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
             mediumBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
@@ -148,6 +178,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     brushDialog.dismiss();
                 }
             });
+
             ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
             largeBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
@@ -157,12 +188,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     brushDialog.dismiss();
                 }
             });
-
             brushDialog.show();
-
         }
-
         else if(view.getId()==R.id.new_btn){
+            //new button
             AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
             newDialog.setTitle("New drawing");
             newDialog.setMessage("Start new drawing (you will lose the current drawing)?");
@@ -179,18 +208,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             });
             newDialog.show();
         }
-
         else if(view.getId()==R.id.save_btn){
+            //save drawing
             AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
             saveDialog.setTitle("Save drawing");
             saveDialog.setMessage("Save drawing to device Gallery?");
             saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
                 public void onClick(DialogInterface dialog, int which){
+                    //save drawing
                     drawView.setDrawingCacheEnabled(true);
+                    //attempt to save
                     String imgSaved = MediaStore.Images.Media.insertImage(
                             getContentResolver(), drawView.getDrawingCache(),
                             UUID.randomUUID().toString()+".png", "drawing");
-
+                    //feedback
                     if(imgSaved!=null){
                         Toast savedToast = Toast.makeText(getApplicationContext(),
                                 "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
@@ -201,7 +232,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
                         unsavedToast.show();
                     }
-
                     drawView.destroyDrawingCache();
                 }
             });
@@ -212,6 +242,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             });
             saveDialog.show();
         }
+        else if(view.getId()==R.id.opacity_btn){
+            //launch opacity chooser
+            final Dialog seekDialog = new Dialog(this);
+            seekDialog.setTitle("Opacity level:");
+            seekDialog.setContentView(R.layout.opacity_chooser);
+            //get ui elements
+            final TextView seekTxt = (TextView)seekDialog.findViewById(R.id.opq_txt);
+            final SeekBar seekOpq = (SeekBar)seekDialog.findViewById(R.id.opacity_seek);
+            //set max
+            seekOpq.setMax(100);
+            //show current level
+            int currLevel = drawView.getPaintAlpha();
+            seekTxt.setText(currLevel+"%");
+            seekOpq.setProgress(currLevel);
+            //update as user interacts
+            seekOpq.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    seekTxt.setText(Integer.toString(progress)+"%");
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+
+            });
+            //listen for clicks on ok
+            Button opqBtn = (Button)seekDialog.findViewById(R.id.opq_ok);
+            opqBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setPaintAlpha(seekOpq.getProgress());
+                    seekDialog.dismiss();
+                }
+            });
+            //show dialog
+            seekDialog.show();
+        }
     }
 }
